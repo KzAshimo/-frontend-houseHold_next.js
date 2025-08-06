e7a4f04cdc1510bb1b17b991533b152090d461e6
KzAshimo <good.ashimo@gmail.com>
2025-08-06 22:42:56 +0900

    useExpenseIndexHook作成 ExpenseTable(composent)たたき台作成

---
+++ features/expense/hooks/useIndexHook.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import axios from "@/lib/axios";
+import useSWR from "swr";
+
+type Expense = {
+  id: number;
+  user: string;
+  category: string;
+  amount: number;
+  memo: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type ApiResponse<T> = { data: T };
+type ErrorResponse = { message: string };
+
+const fetcher = (url: string) =>
+  axios
+    .get<ApiResponse<Expense[]>>(url, { withCredentials: true })
+    .then((res) => res.data.data);
+
+const useExpenseIndex = () => {
+  const { data, error, isLoading, mutate } = useSWR<Expense[], ErrorResponse>(
+    "/api/v1/expense/index",
+    fetcher,
+    { revalidateOnFocus: false }
+  );
+
+  return {
+    expenses: data ?? [],
+    isLoading,
+    error: error ? error.message : null,
+    refetch: mutate,
+  };
+};
+
+export default useExpenseIndex;
