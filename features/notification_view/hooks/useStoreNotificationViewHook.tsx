fb669bd6d052aeb9f8596363ae85c43c14e469b8
KzAshimo <good.ashimo@gmail.com>
2025-08-20 19:24:57 +0900

    view index / store 作成

---
+++ features/notification_view/hooks/useStoreNotificationViewHook.tsx
@@ -0,0 +1,30 @@
+import axiosInstance from "@/lib/axios";
+import handleApiError from "@/lib/handleApiError";
+import { useState } from "react";
+
+type NotificationViewPayload = {
+    userId: number;
+    notificationId: number;
+    viewed_at: string;
+}
+
+const useStoreNotificationView = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const storeNotificationView = async (payLoad: NotificationViewPayload) => {
+        setIsLoading(true);
+        setError(null);
+        try{
+            await axiosInstance.post("/api/v1/notification_view/store", payLoad);
+        }catch(err){
+            setError(handleApiError(err));
+        }finally{
+            setIsLoading(false);
+        }
+    };
+
+    return {storeNotificationView, isLoading, error};
+};
+
+export default useStoreNotificationView;
