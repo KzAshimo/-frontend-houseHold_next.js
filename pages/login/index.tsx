9a94ec92a02cec0a17bf1367e28901088b03d35b
KzAshimo <good.ashimo@gmail.com>
2025-08-23 23:14:50 +0900

    dashboard 背景変更

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,7 +3,7 @@ import "../../app/globals.css";
 
 const LoginPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center px-4 bg-cover bg-center bg-[url('/image/login.jpg')] relative">
+    <div className="min-h-screen flex items-center justify-center px-4 bg-cover bg-center bg-[url('/image/login.jpg')] relative bg-black/70">
       <div className="absolute inset-0" />
       <div className="relative bg-slate-500/40 shadow-md rounded-xl p-8 w-full max-w-md">
         <h1 className="text-2xl font-bold mb-6 text-center text-black">
