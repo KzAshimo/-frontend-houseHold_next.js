b0a0889c762c63427c1f86993a43716f83235aa7
KzAshimo <good.ashimo@gmail.com>
2025-08-25 21:39:48 +0900

    storeHokk 作成

--- features/auth/hooks/useStoreUserHook.tsx
+++ features/auth/hooks/useStoreUserHook.tsx
@@ -3,33 +3,32 @@ import handleApiError from "@/lib/handleApiError";
 import { useState } from "react";
 
 type UserPayload = {
-    name: string;
-    email: string;
-    password: string;
-    role: string;
-}
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+};
 
 const useStoreUserHook = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-    const storeUser = async (payload: UserPayload) => {
-        setIsLoading(true);
-        setError(null);
-        try{
-            await axiosInstance.post("/api/v1/user/store", payload);
-        }catch(err){
-            setError(handleApiError(err));
-        }finally{
-            setIsLoading(false);
-        }
-    };
+  const storeUser = async (payload: UserPayload) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      await axiosInstance.post("/api/v1/user/store", payload);
+    } catch (err) {
+      setError(handleApiError(err));
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return {
-        storeUser,
-        isLoading,
-        error,
-    };
-
-}
+  return {
+    storeUser,
+    isLoading,
+    error,
+  };
+};
 export default useStoreUserHook;
